344ce0dda5340307b2b9aa138a52f630ed5c4b23
soyu <satori562003@gmail.com>
2024-07-15 11:41:41 +0700

    feature: update last version of project

---
+++ Server/src/java/Utility/Constant.java
@@ -0,0 +1,14 @@
+
+package Utility;
+
+public class Constant {
+
+    public static String GOOGLE_CLIENT_ID = "184865787602-fk1acjjmovuleg2r4i7dabhs6k1bk6lo.apps.googleusercontent.com";
+    public static String GOOGLE_CLIENT_SECRET = "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";
+    public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/server/google_login";
+    public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+    public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+    public static String GOOGLE_GRANT_TYPE = "authorization_code";
+    
+//    public static String GOOGLE_SCOPE = "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/user.phonenumbers.read";
+}
